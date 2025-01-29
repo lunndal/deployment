423e87e0aa91e65fd9f55e7c24271702fc6d4a0a
Terje With Lunndal <terje@lunndal.priv.no>
2025-01-29 15:19:28 +0100

    tweaks

--- Terje-Setup-BOOTSTRAP.ps1
+++ Terje-Setup-BOOTSTRAP.ps1
@@ -1,4 +1,16 @@
-Set-ExecutionPolicy Bypass
+Start-Transcript -Path "C:\Windows\Temp\terje-setup.log" -Append
+[console]::beep(500,300)
+Read-Host -Prompt "Press Enter to view network config"
+Start-Process ipconfig -Wait -NoNewWindow
+Read-Host -Prompt "Press Enter to ping vg.no"
+Start-Process ping -ArgumentList "vg.no"  -Wait -NoNewWindow
+Read-Host -Prompt "Press Enter to set execution policy to Bypass|"
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope Process -Verbose
+Read-Host -Prompt "Press Enter to set variable scriptURL"
 $scriptUrl = "https://raw.githubusercontent.com/lunndal/deployment/refs/heads/main/Terje-Setup.ps1"
-iex (iwr $scriptUrl).content
-Set-ExecutionPolicy RemoteSigned
+Read-Host -Prompt "scriptUrl set to $scriptUrl - Press Enter to install packages..."
+Invoke-Expression (Invoke-WebRequest -UseBasicParsing $scriptUrl).content -Verbose
+Read-Host -Prompt "Packages installed - Press Enter to continue..."
+Stop-Transcript
+
+# iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
--- Terje-Setup.ps1
+++ Terje-Setup.ps1
@@ -29,7 +29,9 @@
 
 # Install chocolatey (as admin!)
 # DEBUG
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 
 #
