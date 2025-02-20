48371f4301a6f406362cd165f1c608cee39a2883
Terje With Lunndal <terje@lunndal.priv.no>
2025-02-20 09:09:36 +0100

    ground up testing

--- Terje-Setup-oobeSystem.ps1
+++ Terje-Setup-oobeSystem.ps1
@@ -1,24 +1,24 @@
 
 # Source 
-# https://raw.githubusercontent.com/lunndal/deployment/refs/heads/main/Terje-Setup-oobeSystem.ps1
+# https://raw.githubusercontent.com/lunndal/deployment/refs/heads/main/Terje-Setup-Audit.ps1
 
 #
 # USAGE
-# Run this script during the "oobeSystem" phase from autounattend.xml
+# Run this script during the "Audit" phase from autounattend.xml
 # Commands will run in the context of the first admin user created, when the installer automatically logs the user in.
 #
 # PURPOSE
 # Global software installation go in this file. After this script is run, the following happens:
 # 1. The autounattend will clone the Private git repo, and we can subsequentally 
 #    run the rest of the scripts from local disk.
-# 2. The Terje-Setup-oobeSystem-FirstLogon.ps1 will run, and perform configuration intende for the
+# 2. The Terje-Setup-Audit-FirstLogon.ps1 will run, and perform configuration intende for the
 #    first defined admin user form the unattend file.
 #
 # NOTE
 # This script will count against the "FirstRun" for the first configured admin user, but it's solely for
 # installing software pre Git clone of Private repo. Later in this phase, after cloning the repo, 
 # any other FirstRun commands for the first logged in admin user will be run from the 
-# Terje-Setup-oobeSystem-FirstLogon.ps1 script.
+# Terje-Setup-Audit-FirstLogon.ps1 script.
 # All other users will instead run the Terje-Setup-NewUsers-FirstLogon.ps1 script.
 # 
 
@@ -31,7 +31,7 @@
 #
 
 $logDir = "C:\Windows\Setup\Scripts"
-$logFile = "$($logDir)\Terje-Setup-oobeSystem-$($env:USERNAME).log"
+$logFile = "$($logDir)\Terje-Setup-Audit-$($env:USERNAME).log"
 <# 
 $chocoApps = @"
 powertoys
@@ -66,54 +66,61 @@ greenshot
 "@
  #>
 
- $chocoApps = @{
-    "powertoys"                     = "/silent"
-    "notepadplusplus"               = ""
-    "choco-cleaner"                 = ""
-    "choco-upgrade-all-at-startup"  = ""
-    "1password"                     = "" 
-    "sysinternals"                  = ""
-    "pwsh"                          = ""
-    "git"                           = ""
-    "GoogleChrome"                  = ""
-    "putty"                         = ""
-    "7zip"                          = ""
-    "ffmpeg"                        = ""
-    "fsviewer"                      = ""
-    "logitech-camera-settings"      = ""
-    "paint.net"                     = ""
-    "teamviewer"                    = ""
-    "transgui"                      = ""
-    "treesizefree"                  = ""
-    "vlc"                           = ""
-    "wireshark"                     = ""
-    "yt-dlp"                        = ""
-    "vscode"                        = ""
-    "Firefox"                       = ""
-    "audacious"                     = ""
-    "audacity"                      = ""
-    "asio4all"                      = ""
-    "FoxitReader"                   = ""
-}
+ <# 
+     "powertoys",
+    "notepadplusplus",
+    "choco-cleaner",
+    "choco-upgrade-all-at-startup",
+    "1password", 
+    "sysinternals",
+    "pwsh",
+    "git",
+    "GoogleChrome",
+    #>
+
+
+ $chocoApps = @(
+    "putty",
+    "7zip",
+    "FoxitReader"
+ )
+
+ <# 
+     "ffmpeg",
+    "fsviewer",
+    "logitech-camera-settings",
+    "paint.net",
+    "teamviewer",
+    "transgui",
+    "treesizefree",
+    "vlc",
+    "wireshark",
+    "yt-dlp",
+    "vscode",
+    "Firefox",
+    "audacious",
+    "audacity",
+    "asio4all",
+ #>
 
 # Misbehaving choco packages (popups etc). Should be ran at first proper logon after oobe
 #"protonvpn"                     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG" # These parameters dont work. Proton support issue 3480745.
-#"greenshot"                     = ""
+#"greenshot",
 
 
 
  # Log settings
 $logDir = "C:\Windows\Setup\Scripts"
-#$chocoLog = "$($logDir)\Terje-Setup-oobeSystem-$($env:USERNAME).log"
+#$chocoLog = "$($logDir)\Terje-Setup-Audit-$($env:USERNAME).log"
 
 # Enable logging.
 Start-Transcript -Path $logFile -Append
-Write-Verbose "Starting script in oobeSystem phase."
+Write-Verbose "Starting script in Audit phase."
 
 #
 # Debug handling.
 #
-Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-START oobeSystem phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
+#Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-START Audit phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
 
 
 #
@@ -131,10 +138,11 @@ choco feature enable -n allowGlobalConfirmation
 #$chocoAppsArray = ($chocoApps -split "`n")
 #choco install @chocoAppsArray --log-file=$($chocoLog) --no-progress --yes --no-color --limit-output --ignore-detected-reboot
 
+Join-String
 foreach ($package in $chocoApps.GetEnumerator() ) {
     Write-Verbose "Installing choco package $($package.Key) with params $($package.Value)"
-    $chocoLog = "$($logDir)\Terje-Setup-oobeSystem-$($env:USERNAME)-CHOCO-$($package.Key).log"
-    choco install $package.Key --log-file=$($chocoLog) --no-progress --yes --no-color --limit-output --ignore-detected-reboot --accept-license --install-arguments "$($package.Value)"
+    $chocoLog = "$($logDir)\Terje-Setup-Audit-$($env:USERNAME)-CHOCO-$($package.Key).log"
+    choco install $package.Key --log-file=$($chocoLog) --no-progress --yes --no-color --limit-output --ignore-detected-reboot --accept-license 
 }
 
 # onthespot
@@ -145,7 +153,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lunndal/onthespot-cach
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lunndal/onthespot-cachefix/refs/heads/main/Start-OnTheSpot.ps1.lnk" -OutFile "$($installDir)\Start-OnTheSpot.ps1.lnk" 
 
 
-Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-END oobeSystem phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
+#Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-END Audit phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
 
 # Stop logging.
 Stop-Transcript
