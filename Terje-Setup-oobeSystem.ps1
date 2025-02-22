dbdfa0e8f86d19bad62ed6779f0803d266efc72c
Terje With Lunndal <terje@lunndal.priv.no>
2025-02-22 19:41:50 +0100

    nadsøglkasj

--- Terje-Setup-oobeSystem.ps1
+++ Terje-Setup-oobeSystem.ps1
@@ -1,3 +1,20 @@
+<#
+
+    ██     ██  █████  ██████  ███    ██ ██ ███    ██  ██████  ██ 
+    ██     ██ ██   ██ ██   ██ ████   ██ ██ ████   ██ ██       ██ 
+    ██  █  ██ ███████ ██████  ██ ██  ██ ██ ██ ██  ██ ██   ███ ██ 
+    ██ ███ ██ ██   ██ ██   ██ ██  ██ ██ ██ ██  ██ ██ ██    ██    
+     ███ ███  ██   ██ ██   ██ ██   ████ ██ ██   ████  ██████  ██ 
+
+      -------------------------------------------------------
+      This script is public and should NEVER contain secrets!
+      -------------------------------------------------------
+#>
+
+param (
+    $Token = "NOT_SET"
+)
+
 
 #
 # Declarations
@@ -39,20 +56,23 @@ $chocoApps = @(
 # DEBUG START
 #Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-START Audit phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
 
+"CUSTOM INSTALL STEPS - DO NOT CLOSE THIS WINDOW!" | Out-Host
 
 #
 # Install applications.
 #
 
 # chocolatey (in admin context)
+"Installing chocolatey" | Out-Host
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 
 # Choco applications.  
+"Installing choco apps" | Out-Host
 foreach ($package in $chocoApps) {
-    Write-Host "Installing choco package $($package) with params $($package)"
+    "`tInstalling choco package $($package)" | Out-Host
     $chocoLog = "$($logDir)\Terje-Setup-Audit-$($env:USERNAME)-CHOCO-$($package).log"
     choco install $package --no-progress --yes --no-color --limit-output --ignore-detected-reboot --accept-license 
 }
@@ -62,16 +82,23 @@ foreach ($package in $chocoApps) {
 #
 
 # Close PowerToys welcome screen.
+"Killing PowerToys welcome screen" | Out-Host
 Get-Process | Where-Object { $_.MainWindowTitle -match "powertoys" } |  Select-Object Id, ProcessName, MainWindowTitle | Stop-Process
 # ProtonVPN
 # Greenshot
 
 # onthespot
+"Installing onthespot portable" | Out-Host
 $installDir = (Join-Path $env:ProgramFiles "onthespot")
 New-Item -ItemType Directory -Path $installDir
 Invoke-WebRequest -Uri "https://github.com/casualsnek/onthespot/releases/latest/download/onthespot_win_ffm.exe" -OutFile "$($installDir)\onthespot_win_ffm.exe"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lunndal/onthespot-cachefix/refs/heads/main/Start-OnTheSpot.ps1" -OutFile "$($installDir)\Start-OnTheSpot.ps1" 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lunndal/onthespot-cachefix/refs/heads/main/Start-OnTheSpot.ps1.lnk" -OutFile "$($installDir)\Start-OnTheSpot.ps1.lnk" 
+
+# Clone private repo
+"Cloning private repo with token $($Token)" | Out-Host
+& "C:\Program Files\Git\cmd\git.exe" clone "https://$($Token)@github.com/lunndal/Private.git" "C:\Users\Terje\src\Private"
+
 
 # DEBUG END
 #Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-END Audit phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
--- Terje-Setup-specialize.ps1
+++ Terje-Setup-specialize.ps1
@@ -11,36 +11,17 @@
       -------------------------------------------------------
 #>
 
-# Source 
-# https://raw.githubusercontent.com/lunndal/deployment/refs/heads/main/Terje-Setup-specialize.ps1
-
-
-#
-# USAGE
-# Run this script during the "specialize" phase from autounattend.xml.
-# Commands will run in system context before any users are created, so any user settings will apply to ALL users.
-# 
-
-#
-# ISSUES
-# ? Execution Policy er ikke liberal nok når dette skriptet skal kjøres.
 
 #
 # Declarations
 #
 #$logDir = "C:\Windows\Setup\Scripts"
-#$logFile = "$($logDir)\Terje-Setup-specialize-$($env:USERNAME).log"
 
 
 
-# Enable logging.
-#Start-Transcript -Path $logFile -Append
-#Write-Verbose "Starting script in specialize phase."
-
-#
-# Debug handling.
-#
+# DEBUG
 #Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-START specialize phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
+
 
 #
 # Windows settings.
@@ -48,15 +29,9 @@
 
 
 #
-# Install Chrome extensions
-#
-
-
-#
 # Global application settings
 #
 
+# DEBUG
 #Start-Process powershell -ArgumentList "-NoExit -Command `"Write-Host 'DEBUG-END specialize phase. Exit shell when done debugging.'`"" -Wait -WindowStyle Normal
 
-# Stop logging.
-#Stop-Transcript
